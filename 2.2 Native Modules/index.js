c0003a1315240ee5ac07e72c4dc4ef0ae519d3a7
Enijeshiku Eniola <eenijeshiku@gmail.com>
2024-12-28 22:17:20 +0100

    Changes

--- 2.2 Native Modules/index.js
+++ 2.2 Native Modules/index.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
 
-fs.writeFile("message.txt", "Hello from Henney!", (err) => {
+//fs.writeFile("message.txt", "Hello from Henney!", (err) => {
+  //  if (err) throw err;
+    // console.log("File saved!"); 
+// }); 
+
+fs.readFile("./message.txt", "utf8", (err, data) => {
     if (err) throw err;
-    console.log("File saved!"); 
-}); 
+    console.log(data);
+  }); 
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # UDEMY_Projects-Repo
 
 ## Staring with the backend 
+
+### 2.1
 ![Page Screenshot](/Images/Screenshot%20(156).png)
+
+### 2.2
+![Page Screenshot](/Images/Screenshot%20(157).png)
+
